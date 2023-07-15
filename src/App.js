d0d2a04f575e5481704c7887543570fad81ee122
MrHuezo <fractalestudiomx@egmail.com>
2023-07-15 13:32:59 -0500

    implementing Sheet Diego

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
+import SnackBarMessages from './SnackBarMessages';
 import Typography from '@mui/material/Typography';
 
 import { createTheme, ThemeProvider, styled} from '@mui/material/styles';
@@ -40,36 +41,58 @@ const WhiteBorderTextField = styled(TextField)`
 `;
 
 const defaultTheme = createTheme();
+
 function App() {
+   const [loading, setLoading] = useState(false);
  
-  const enviarDatos = async (event) => {
-    try {
+   const [datosObjectError, setDatosObject] = useState({nombre:false, tel:false, fecha_entrada:false, fecha_salida:false });
+   const [alert, setAlert] = useState({ message: "", status: "", open: false });
+   const formElement = useRef();
+  
+   const enviarDatos = async (event) => {
       event.preventDefault();
+    
+      var frm = document.getElementsByName('formHere')[0];
+      
+      setLoading(true);
       const data = new FormData(event.currentTarget);
+
       let bodyFormData = new FormData();
-     
-     
+      let datosObjectSend = {nombre:false, tel:false, fecha_entrada:false, fecha_salida:false};
+      if(data.get("nombre") === "" ) datosObjectSend.nombre = true;
+      if(data.get("tel") === "" ) datosObjectSend.tel = true;
+      if(data.get("fecha_entrada") === "" ) datosObjectSend.fecha_entrada = true;
+      if(data.get("fecha_salida") === "" ) datosObjectSend.fecha_salida = true;
+
+      setDatosObject(datosObjectSend);
+      if(datosObjectSend.nombre || datosObjectSend.tel ||datosObjectSend.fecha_entrada ||datosObjectSend.fecha_salida) {
+        setAlert({ message: "Complete los campos obligatorios.", status: "error", open: true });
+      
+        setLoading(false);
+        return;
+      }
+      
       bodyFormData.append('nombre', data.get("nombre"));
       bodyFormData.append('tel', data.get("tel"));
       bodyFormData.append('fecha_entrada', data.get("fecha_entrada"));
       bodyFormData.append('fecha_salida', data.get("fecha_salida"));
-     console.log(bodyFormData)
+  
    
       await axios({
         method: "POST",
-        url:process.env.SCRIPT_GOOGLE_ONE,
+        url: process.env.REACT_APP_GOOGLE_SCRIPT_ONE,
         headers: { "Content-Type": "multipart/form-data" },
         data:bodyFormData,
       
       }).then(response => {
-        console.log(response)
+        
+        setAlert({ message: response.data, status: "success", open: true });
+        setTimeout(window.location.reload(false), 5);
       }).catch(error =>{
-        console.log(error)
-      })
-      
-    } catch (error) {
       console.log(error)
-    }
+        setLoading(false);
+      })
+   setLoading(false);
   };
   return (
 
@@ -77,6 +100,7 @@ function App() {
        <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: '100vh',display:"flex",alignItems: 'center', backgroundColor:'black' }}>
         <CssBaseline />
+        <SnackBarMessages alert={alert} setAlert={setAlert} />
         <Grid
           item
           xs={false}
@@ -84,7 +108,7 @@ function App() {
           sm={4}
           md={6}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: 'url(https://source.unsplash.com/random?calendar)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -93,7 +117,7 @@ function App() {
               height: '100vh'
           }}
         />
-        <Grid item  alignContent="center" sm={8} md={6}  square>
+        <Grid item  alignContent="center" sm={8} md={6}  >
           <Box
             sx={{
               my: 2,
@@ -107,7 +131,7 @@ function App() {
             <Typography component="h1" variant="h5" color="white">
              Reservación
             </Typography>
-            <Box component="form" noValidate justifyContent="center" onSubmit={enviarDatos} sx={{ mt: 1 }}>
+            <Box component="form" name="formHere" ref={formElement} noValidate justifyContent="center" onSubmit={enviarDatos} sx={{ mt: 1 }}>
               <Box  mt={1}>
                 <WhiteBorderTextField
                   fullWidth
@@ -121,11 +145,14 @@ function App() {
                   }}
                   type="text"
                   size="small"
+                  required
                   name="nombre"
                   id="nombre"
                   label="Nombre"
                   margin="normal"
                   autoFocus
+                  error={datosObjectError.nombre }
+                  inputProps= {{ style: { textTransform: "uppercase" } }}
                   variant="outlined"
                   mt={1}
                 />
@@ -139,7 +166,9 @@ function App() {
                   id="tel"
                   label="Teléfono"
                   variant="outlined"
-
+                  required
+                  error={datosObjectError.tel }
+                  inputProps= {{ style: { textTransform: "uppercase" } }}
                   sx={{ "& .MuiInputLabel-root": {color: 'white'},//styles the label
                     "& .MuiOutlinedInput-root": {
                       "& > fieldset": { borderColor: "white" },
@@ -157,11 +186,16 @@ function App() {
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker   format="DD-MM-YYYY"
                      sx={{color:"white"}}
+                     
                      slotProps={{ 
                       textField: 
                         { id:"fecha_entrada", 
                           name:"fecha_entrada", 
+                          required:true,
+                          inputProps: { style: { textTransform: "uppercase" } },
                           label:"Fecha entrada" ,
+                          
+                          error:datosObjectError.fecha_entrada,
                           size: 'small', 
                           sx:{color:"white", "& .MuiInputLabel-root": {color: 'white'},//styles the label
                             "& .MuiOutlinedInput-root": {
@@ -181,14 +215,18 @@ function App() {
                     <DatePicker  
                       format="DD-MM-YYYY"
                       colorIcon={"white"}
+                      
                       keyboardIcon={<CalendarMonthIcon color="white"/>}
                       slotProps={{ 
                         icon:{color:"white"},
                         textField: { 
                           id:"fecha_salida", 
+                          inputProps: { style: { textTransform: "uppercase" } },
                           name:"fecha_salida",
                           label:"Fecha salida" ,
+                          required:true,
                           size: 'small',
+                          error:datosObjectError.fecha_salida  ,
                           sx:{color:"white", "& .MuiInputLabel-root": {color: 'white'},//styles the label
                               "& .MuiOutlinedInput-root": {
                                 "& > fieldset": { borderColor: "white" },
@@ -204,14 +242,30 @@ function App() {
                 </Grid>
              </Grid> 
               <Box >
+              {
+                loading ?
                 <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
+                  disabled
+                  variant="outlined"
                   sx={{ mt: 3, mb: 2 }}
                 >
-                  Enviar 
-                </Button>      
+                <CircularProgress color="inherit" size={22} sx={{ mt: 5, mb: 2, color:"white" }}/>
+              </Button> 
+                
+                :
+                <Button
+                type="submit"
+                fullWidth
+               
+                variant="outlined"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Enviar 
+              </Button> 
+              }
+              
+              
+                    
               </Box>
             </Box>
           </Box>
